aef6a0d46142932e443cf275f8728f8f93530534
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-16 20:30:57 +0300

    exercise 2

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,8 @@
 class RatingsController < ApplicationController
+   
     def index
+        @ratings = Rating.all
     end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,18 @@ b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1040
 
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-b3.beers.create name:"Hefeweizen", style:"Weizen"
-b3.beers.create name:"Helles", style:"Lager"
+br1 = b1.beers.create name:"Iso 3", style:"Lager"
+br2 = b1.beers.create name:"Karhu", style:"Lager"
+br3 = b1.beers.create name:"Tuplahumala", style:"Lager"
+br4 = b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
+br5 = b2.beers.create name:"X Porter", style:"Porter"
+br6 = b3.beers.create name:"Hefeweizen", style:"Weizen"
+br7 = b3.beers.create name:"Helles", style:"Lager"
+
+br1.ratings.create score: 24
+br2.ratings.create score: 5
+br3.ratings.create score: 12
+br4.ratings.create score: 35
+br5.ratings.create score: 22
+br6.ratings.create score: 17
+br7.ratings.create score: 35
