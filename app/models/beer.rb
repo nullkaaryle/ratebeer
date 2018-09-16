0cb6617bd1089a21a622892180b3f5a723bfef53
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-16 21:22:48 +0300

    exercise 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,18 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum = 0.0
+        count = 0
+        ratings = Rating.where(beer_id: self.id) 
+
+        ratings.each do |rating|
+            sum = sum + rating.score
+            count = count + 1
+        end
+
+        sum / count
+    end
+
 end
