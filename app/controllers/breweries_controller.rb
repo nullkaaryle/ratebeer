f70823e9d188f5eab9b3b55827b32147701aab51
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-22 20:00:02 +0300

    Week 2 done

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,8 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:destroy]
+  
+  
 
   # GET /breweries
   # GET /breweries.json
@@ -62,7 +65,8 @@ class BreweriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    
+  # Use callbacks to share common setup or constraints between actions.
     def set_brewery
       @brewery = Brewery.find(params[:id])
     end
@@ -71,4 +75,15 @@ class BreweriesController < ApplicationController
     def brewery_params
       params.require(:brewery).permit(:name, :year)
     end
+
+
+    def authenticate
+      admin_accounts = { "pekka" => "beer", "arto" => "foobar", "matti" => "ittam", "vilma" => "kangas" }
+      
+      authenticate_or_request_with_http_basic do |username, password|
+        admin_accounts[username] == password
+    end
+
+  end
+
 end
