d5ad3b8cc05088a95c4321c2574bc27e7c2f8e5e
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-22 18:40:21 +0300

    Still on week 2 exercises

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,8 @@
 class Beer < ApplicationRecord
+    include RatingAverage
+
     belongs_to :brewery
     has_many :ratings, dependent: :destroy
-
-    def average_rating
-        Rating.where(beer_id: self.id).average(:score)
-    end
 
     def to_s
         "#{self.name} (#{self.brewery.name})"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,16 +1,7 @@
 class Brewery < ApplicationRecord
+    include RatingAverage
+
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
-
-    def print_report
-        puts name
-        puts "established at year #{year}"
-        puts "number of beers #{beers.count}"
-    end
-
-    def restart
-        self.year = 2018
-        puts "changed year to #{year}"
-    end
 
 end
