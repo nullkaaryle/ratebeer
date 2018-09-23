01faee9ddfa5cbf5961af8a9f5f15f5464fa5953
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-23 20:32:38 +0300

    Week 3, exercise 5

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,14 +9,15 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-    redirect_to ratings_path
+    rating = Rating.new params.require(:rating).permit(:score, :beer_id)
+    rating.user = current_user
+    rating.save
+    redirect_to current_user
   end
 
   def destroy
     rating = Rating.find(params[:id])
     rating.delete
-    redirect_to ratings_path
+    redirect_to user_path(current_user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_153009) do
+ActiveRecord::Schema.define(version: 2018_09_23_165624) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_09_23_153009) do
     t.integer "beer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
