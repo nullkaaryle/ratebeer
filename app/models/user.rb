75998aec149e49ef332967cd56fe9b9ec0cd4368
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-23 22:35:32 +0300

    Week 3, exercise 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_many :ratings
+  has_many :beers, through: :ratings
+  has_secure_password
+
   validates :username,  uniqueness: true,
                         length: { minimum: 3, maximum: 30 }
 
-  has_many :ratings
+  validates :password,  length: { minimum: 4 },
+                        presence: true,
+                        format: { with: /([A-Z]+.*\d+)|(\d+.*[A-Z]+)/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_165624) do
+ActiveRecord::Schema.define(version: 2018_09_23_184843) do
 
   create_table "beers", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_09_23_165624) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
