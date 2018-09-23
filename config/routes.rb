96ce334c3cc3a69c1449ddaee48966766bf72f20
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-23 19:35:49 +0300

    Week 3, excercise 4

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :ratings, only: [:index, :new, :create, :destroy]
   resources :users
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
 
