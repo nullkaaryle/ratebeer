eb5ed02ce355aa733b705c311fc7035605f346bb
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-26 15:49:22 +0300

    Week 3, exercise 8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,7 @@ class Brewery < ApplicationRecord
   has_many :ratings, through: :beers
 
   validates :year, numericality: { greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2018,
+                                   less_than_or_equal_to: ->(_) { Time.now.year },
                                    only_integer: true }
   validates :name, presence: true
 end
