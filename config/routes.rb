39963c38da47dd1156b3cc8547796811450304c1
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-09-30 13:08:50 +0300

    Week 4, capybara

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  resources :memberships
-  resources :beer_clubs
-  root 'breweries#index'
-  resource :session, only: [:new, :create, :destroy]
   resources :beers
+  resources :beer_clubs
   resources :breweries
+  resources :memberships
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :users
-  get 'signup', to: 'users#new'
+
   get 'signin', to: 'sessions#new'
+  get 'signup', to: 'users#new'
   delete 'signout', to: 'sessions#destroy'
+
+  root 'breweries#index'
 end
 
