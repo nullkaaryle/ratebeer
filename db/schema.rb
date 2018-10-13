d35b2cb991a60163abd21a42b2598cc6a7ced1e4
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-10-13 18:33:53 +0300

    add styles to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_133248) do
+ActiveRecord::Schema.define(version: 2018_10_13_150818) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 2018_09_26_133248) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
