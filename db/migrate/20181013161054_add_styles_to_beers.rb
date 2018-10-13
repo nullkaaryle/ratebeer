9cfb0e671d46cb898c6e400693061235893ff0b4
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-10-13 19:12:32 +0300

    add new styles column to tables

---
+++ db/migrate/20181013161054_add_styles_to_beers.rb
@@ -0,0 +1,5 @@
+class AddStylesToBeers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :beers, :style_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_154600) do
+ActiveRecord::Schema.define(version: 2018_10_13_161054) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_13_154600) do
     t.integer "brewery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "style_id"
   end
 
   create_table "breweries", force: :cascade do |t|
