7d0444c74a3018ebe48c6cba22cc192e7334975f
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-10-20 15:09:44 +0300

    Week 7, exercise 1

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,11 +3,19 @@ class BeersController < ApplicationController
   before_action :set_breweries_and_styles_for_template, only: [:new, :edit, :create]
   before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :current_user_admin, only: [:destroy]
-  
+
   # GET /beers
   # GET /beers.json
   def index
     @beers = Beer.all
+
+    order = params[:order] || 'name'
+
+    @beers =  case order
+              when 'name' then @beers.sort_by(&:name)
+              when 'brewery' then @beers.sort_by{ |b| b.brewery.name }
+              when 'style' then @beers.sort_by{ |b| b.style.name }
+              end
   end
 
   # GET /beers/1
