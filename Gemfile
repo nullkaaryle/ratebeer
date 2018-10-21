853778396371da5a831babf3c0aa41605d9a2a49
nullkaaryle <22505837+nullkaaryle@users.noreply.github.com>
2018-10-21 13:47:50 +0300

    Add user index

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bootstrap', '~> 4.1.3'
 
 gem 'jquery-rails'
 
+gem 'rack-mini-profiler'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_17_135501) do
+ActiveRecord::Schema.define(version: 2018_10_20_185305) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_10_17_135501) do
     t.string "password_digest"
     t.boolean "admin"
     t.boolean "closed"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
